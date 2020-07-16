f5daf50375296c12666bc191e8f8cf6c59189767
Chamith <csperamune@gmail.com>
2020-07-16 22:21:41 +0530

    lab

---
+++ Lab01/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class Form1 : Form
+    {
+        preProcessing process = new preProcessing();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            process.loadOriginalImage();
+            pictureBox1.ImageLocation = "1.jpg";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            process.grayScaleImage();
+            pictureBox2.ImageLocation = "2.jpg";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Height: " + process.extractImageHeight(Height) + ",  Width: " + process.extractImageWight(Width) + ", Channels: " + process.extractImageChannels(Height));
+        }
+    }
+}
---
+++ Lab01/preProcessing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace Lab01
+{
+    class preProcessing
+    {
+        IplImage src;
+        private CvArr dst;
+
+
+        public void loadOriginalImage()
+        {
+            src = Cv.LoadImage("eye.jpg", LoadMode.Color);
+            Cv.SaveImage("1.jpg", src);
+        }
+        public void grayScaleImage()
+        {
+            dst = Cv.LoadImage("1.jpg", LoadMode.GrayScale);
+            Cv.CvtColor(src, dst, ColorConversion.RgbToGray);
+            Cv.SaveImage("2.jpg", dst);
+        }
+        public int extractImageWight(int size)
+        {
+            return src.Width;
+        }
+
+        public int extractImageHeight(int Height)
+        {
+            return src.Height;
+        }
+
+        public int extractImageChannels(int ElemChannels)
+        {
+            return src.ElemChannels;
+        }
+    }
+}
